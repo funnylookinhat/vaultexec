993bb2e3c1a5b8190d479bf2c63e3ea9fd5f32de
David Overcash <funnylookinhat@gmail.com>
2018-02-25 16:50:46 -0800

    Add periodic token renewal.

--- vault.go
+++ vault.go
@@ -27,20 +27,16 @@ type VaultSecretResponse struct {
 	// The data that comes back for secrets can be of any type, but the keys will
 	// always be strings.  So rather than have map[string]string, which fails to
 	// unmarshal, we just use map[string]interface{}
-	Data          map[string]interface{} `json:"data"`
-	LeaseDuration int64                  `json:"lease_duration"`
+	Data map[string]interface{} `json:"data"`
 }
 
 // VaultRenewResponse handles fields we care about from renewing the token.
-// TODO - Use in a separate follow-up PR.
-/*
 type VaultRenewResponse struct {
 	Errors []string `json:"errors"`
 	Auth   struct {
 		LeaseDuration int64 `json:"lease_duration"`
 	}
 }
-*/
 
 // GenerateVaultConfig using arguments and environment variables: VAULT_ADDR,
 // VAULT_TOKEN, and VAULT_PATH
@@ -88,13 +84,13 @@ func GenerateVaultConfig(address *string, token *string, path *string) (VaultCon
 	return config, nil
 }
 
-// GetVaultSecrets fetches secrets from vault and returns a map[string]interface{}
-func GetVaultSecrets(config VaultConfig) (map[string]interface{}, error) {
+// Make a request to the vault service with a given method.
+func makeVaultRequest(method string, path string, config VaultConfig) ([]byte, error) {
 	client := &http.Client{}
 
-	requestURL := fmt.Sprintf("%s/v1/%s", config.Address, config.Path)
+	requestURL := fmt.Sprintf("%s/%s", config.Address, path)
 
-	req, err := http.NewRequest("GET", requestURL, nil)
+	req, err := http.NewRequest(method, requestURL, nil)
 
 	if err != nil {
 		return nil, err
@@ -126,6 +122,17 @@ func GetVaultSecrets(config VaultConfig) (map[string]interface{}, error) {
 			resp.StatusCode)
 	}
 
+	return bodyBytes, nil
+}
+
+// GetVaultSecrets fetches secrets from vault and returns a map[string]interface{}
+func GetVaultSecrets(config VaultConfig) (map[string]interface{}, error) {
+	bodyBytes, err := makeVaultRequest("GET", "v1/"+config.Path, config)
+
+	if err != nil {
+		return nil, err
+	}
+
 	var vaultSecretResponse VaultSecretResponse
 
 	err = json.Unmarshal(bodyBytes, &vaultSecretResponse)
@@ -136,12 +143,35 @@ func GetVaultSecrets(config VaultConfig) (map[string]interface{}, error) {
 
 	if len(vaultSecretResponse.Errors) > 0 {
 		return nil, fmt.Errorf(
-			"vault server error (HTTP status %d): %s",
-			resp.StatusCode,
+			"vault server error: %s",
 			strings.Join(vaultSecretResponse.Errors, ","))
 	}
 
-	// TODO - Refactor to return the entire secret response so that we can start
-	// renewing the token periodically
 	return vaultSecretResponse.Data, nil
+}
+
+// RenewVaultToken attempts to renew the token provided in the config, returns
+// the lease expiration and an error.
+func RenewVaultToken(config VaultConfig) (int64, error) {
+	bodyBytes, err := makeVaultRequest("POST", "v1/auth/token/renew-self", config)
+
+	if err != nil {
+		return 0, err
+	}
+
+	var vaultRenewResponse VaultRenewResponse
+
+	err = json.Unmarshal(bodyBytes, &vaultRenewResponse)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if len(vaultRenewResponse.Errors) > 0 {
+		return 0, fmt.Errorf(
+			"vault server error: %s",
+			strings.Join(vaultRenewResponse.Errors, ","))
+	}
+
+	return vaultRenewResponse.Auth.LeaseDuration, nil
 }
